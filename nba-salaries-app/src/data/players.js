0a50a7ca8589b9dbec53c18a43dc88c6e4ecb98e
Yodae Lim <yodae.lim@gmail.com>
2022-02-10 12:38:37 -0800

    make dummy player data

---
+++ nba-salaries-app/src/data/players.js
@@ -0,0 +1,21 @@
+const players = [
+    {
+        player_ID: 1,
+        player_first_name: "Lebron",
+        player_last_name: "James",
+        player_birth_date: new Date(1984, 12, 30),
+        player_number: 23,
+        player_team_ID: 1,
+        player_Positions_ID: null
+    },
+    {
+        player_ID: 2,
+        player_first_name: "Anthony",
+        player_last_name: "Davis",
+        player_birth_date: new Date(1993, 3, 11),
+        player_number: 3,
+        player_team_ID: 1,
+        player_Positions_ID: null
+    },
+
+]
